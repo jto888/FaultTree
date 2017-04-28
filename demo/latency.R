fe898717f9e7a439204f252f9b6a87d9e90bfdd1
Jacob Ormerod <jake@openreliability.org>
2017-04-28 00:26:03 -0400

    remove pzero args from latency demo
    Yes, the tutorial has them for now, and I have proven the code works (by
    ignoring the depreciated argument), but this demo should have longer
    life.

--- demo/latency.R
+++ demo/latency.R
@@ -19,9 +19,9 @@ hf<-addDemand(hf, at=1, mttf=1e6, name= "Vaporizer Rupture", name2="Due to Stres
 hf<-addLogic(hf, at=2, type="or", name="Pressure Relief System", name2="in Failed State")
 hf<-addLogic(hf, at=2, type="or", name="Overpressure", name2="Occurs")
 hf<-addLogic(hf, at=4, type="or", name="Pressure Relief", name2="Isolated")
-hf<-addLatent(hf, at=6, mttf=10, pzero=0, inspect=walkby,
+hf<-addLatent(hf, at=6, mttf=10, inspect=walkby,
 name="Valve 20", name2="Left Closed")
-hf<-addLatent(hf, at=6, mttf=10, pzero=0, inspect=walkby, display_under=7,
+hf<-addLatent(hf, at=6, mttf=10, inspect=walkby, display_under=7,
 name="Valve 21", name2="Left Closed")
 hf<-addLogic(hf, at=4, type="or", name="Rupture Disk Fails", name2="to Open at Design Pt.")
 hf<-addLogic(hf, at=9, type="or", name="Installation/Mfr", name2="Errors")
@@ -32,20 +32,20 @@ hf<-addProbability(hf, at=10, prob=.001, display_under=12,
 name="Rupture Disk", name2="Manuf. Error")
 hf<-addLogic(hf, at=9, type="or", name="Pressure Between Disk", name2="and Relief Valve")
 hf<-addLogic(hf, at=14, type="inhibit", name="Pressure NOT" , name2="Detectable by PI")
-hf<-addLatent(hf, at=15, mttf=10, pzero=0, inspect=pi_test,
+hf<-addLatent(hf, at=15, mttf=10, inspect=pi_test,
 name="Pressure Gage", name2="Failed Low Position")
-hf<-addLatent(hf, at=15, mttf=10, pzero=0, inspect=pi_test,
+hf<-addLatent(hf, at=15, mttf=10, inspect=pi_test,
 name="Rupture Disk Leak", name2="Undetected")
 hf<-addLogic(hf, at=14, type="inhibit", name="Pressure" , name2="Detectable by PI")
 hf<-addProbability(hf, at=18, prob=(1-hf$PBF[16]),
 name="Pressure Gage", name2="Detects Pressure")
-hf<-addLatent(hf, at=18, mttf=10, pzero=0, inspect=walkby,
+hf<-addLatent(hf, at=18, mttf=10, inspect=walkby,
 name="Rupture Disk Leak", name2="Detectable")
 hf<-addLogic(hf, at=4, type="or",
 name="Pressure Relief Fails", name2=" to Open at Design Pt")
-hf<-addLatent(hf, at=21, mttf=300, pzero=0, inspect=rv_test,
+hf<-addLatent(hf, at=21, mttf=300, inspect=rv_test,
 name="Pressure Relief", name2="set too high")
-hf<-addLatent(hf, at=21, mttf=300, pzero=0, inspect=rv_test,
+hf<-addLatent(hf, at=21, mttf=300, inspect=rv_test,
 name="Pressure Relief Unable", name2="to Open at Design Pt")
 hf<-addDemand(hf, at=5, mttf=10, name= "High Pressure", name2="Feed to Vaporizer")
 hf<-addDemand(hf, at=5, mttf=10, name= "Vaporizer Heating", name2="Runaway")
